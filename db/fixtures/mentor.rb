cc91983fde2ee9ef96a6960654048440711c83f8
ponkantaichou <banshee7577@gmail.com>
2020-11-09 12:45:54 +0900

    seed

---
+++ db/fixtures/mentor.rb
@@ -0,0 +1,34 @@
+  Mentor.seed do |s|
+  s.id = 1
+  s.name = '木戸ライフコーチ'
+  end
+  
+  Mentor.seed do |s|
+  s.id = 2
+  s.name = '大河内さん'
+  end
+  
+  Mentor.seed do |s|
+  s.id = 3
+  s.name = '松本さん'
+  end
+
+  Mentor.seed do |s|
+  s.id = 4
+  s.name = '斎藤さん'
+  end
+  
+  Mentor.seed do |s|
+  s.id = 5
+  s.name = '山本さん'
+  end
+  
+  Mentor.seed do |s|
+  s.id = 6
+  s.name = '河合さん'
+  end
+
+  Mentor.seed do |s|
+    s.id = 7
+    s.name = '新規で登録して欲しいメンターさん（星は３でお願いします）'
+    end
